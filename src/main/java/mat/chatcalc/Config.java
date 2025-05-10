60cd3c923081723ac660a98f626e720355b1358b
matcool <26722564+matcool@users.noreply.github.com>
2025-05-10 01:35:42 -0300

    base idea works

--- src/main/java/mat/chatcalc/Config.java
+++ src/main/java/mat/chatcalc/Config.java
@@ -5,15 +5,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your
-// config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
 	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-	private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-		.comment("Whether to log the dirt block on common setup").define("logDirtBlock", true);
+	private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER.comment("Whether to log the dirt block on common setup").define("logDirtBlock", true);
 
 	static final ForgeConfigSpec SPEC = BUILDER.build();
 
--- src/main/java/mat/chatcalc/ExampleMod.java
+++ src/main/java/mat/chatcalc/ExampleMod.java
@@ -1,6 +1,8 @@
 package mat.chatcalc;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,8 +27,13 @@ public class ExampleMod {
 	@SubscribeEvent
 	public void onClientChat(ClientChatEvent event) {
 		if (event.getMessage().startsWith("=")) {
-			// Minecraft.getInstance().player.sendSystemMessage("ChatCalc is running!");
-			LOGGER.info("Hello! " + event.getMessage());
+			var player = Minecraft.getInstance().player;
+			if (player == null) {
+				return;
+			}
+			var input = event.getMessage().substring(1).trim();
+			player.sendSystemMessage(Component.literal("= " + input));
+			player.sendSystemMessage(Component.literal("§7§oumm idk"));
 			event.setCanceled(true);
 		}
 	}
